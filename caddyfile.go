8f8e9743e2f0dead5dd10ace0c7b4b55fb0cce19
Alan Pearce <alan@alanpearce.eu>
2024-11-30 16:46:36 +0100

    Remove caddy compatibility (certmagic only)

--- caddyfile.go
+++ caddyfile.go
@@ -2,168 +2,18 @@ package storageredis
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"net"
-	"reflect"
 	"strconv"
 
-	"github.com/spf13/cobra"
 	"go.uber.org/zap"
-
-	"github.com/caddyserver/caddy/v2"
-	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
-	caddycmd "github.com/caddyserver/caddy/v2/cmd"
-	"github.com/caddyserver/certmagic"
 )
 
-func init() {
-	caddy.RegisterModule(RedisStorage{})
-	caddycmd.RegisterCommand(caddycmd.Command{
-		Name:  "redis",
-		Short: "Commands for working with the Caddy Redis Storage module",
-		CobraFunc: func(cmd *cobra.Command) {
-			rebuildCmd := &cobra.Command{
-				Use:   "repair --config <path>",
-				Short: "Repair the Redis Storage directory index tree",
-				RunE:  caddycmd.WrapCommandFuncForCobra(cmdRedisStorageRepair),
-			}
-			rebuildCmd.Flags().StringP("config", "c", "", "Caddy configuration file (optional)")
-			cmd.AddCommand(rebuildCmd)
-		},
-	})
-}
+func (rs *RedisStorage) ProvisionCertMagic(ctx context.Context, log *zap.Logger) error {
+	rs.logger = log
 
-func (RedisStorage) CaddyModule() caddy.ModuleInfo {
-	return caddy.ModuleInfo{
-		ID: "caddy.storage.redis",
-		New: func() caddy.Module {
-			return New()
-		},
-	}
-}
-
-func (rs *RedisStorage) CertMagicStorage() (certmagic.Storage, error) {
-	return rs, nil
-}
-
-func (rs *RedisStorage) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
-
-	for d.Next() {
-
-		// Optional Redis client type either "cluster" or "failover"
-		if d.NextArg() {
-			val := d.Val()
-			if val == "cluster" || val == "failover" {
-				rs.ClientType = val
-			} else {
-				return d.ArgErr()
-			}
-		}
-
-		for nesting := d.Nesting(); d.NextBlock(nesting); {
-			configKey := d.Val()
-			var configVal []string
-
-			if d.NextArg() {
-				// configuration item with single parameter
-				configVal = append(configVal, d.Val())
-			} else {
-				// configuration item with nested parameter list
-				for nesting := d.Nesting(); d.NextBlock(nesting); {
-					configVal = append(configVal, d.Val())
-				}
-			}
-			// There are no valid configurations where configVal slice is empty
-			if len(configVal) == 0 {
-				return d.Errf("no value supplied for configuraton key '%s'", configKey)
-			}
-
-			switch configKey {
-			case "address":
-				rs.Address = configVal
-			case "host":
-				rs.Host = configVal
-			case "port":
-				rs.Port = configVal
-			case "db":
-				dbParse, err := strconv.Atoi(configVal[0])
-				if err != nil {
-					return d.Errf("invalid db value: %s", configVal[0])
-				}
-				rs.DB = dbParse
-			case "timeout":
-				rs.Timeout = configVal[0]
-			case "username":
-				if configVal[0] != "" {
-					rs.Username = configVal[0]
-				}
-			case "password":
-				if configVal[0] != "" {
-					rs.Password = configVal[0]
-				}
-			case "master_name":
-				if configVal[0] != "" {
-					rs.MasterName = configVal[0]
-				}
-			case "key_prefix":
-				if configVal[0] != "" {
-					rs.KeyPrefix = configVal[0]
-				}
-			case "encryption_key", "aes_key":
-				rs.EncryptionKey = configVal[0]
-			case "compression":
-				Compression, err := strconv.ParseBool(configVal[0])
-				if err != nil {
-					return d.Errf("invalid boolean value for 'compression': %s", configVal[0])
-				}
-				rs.Compression = Compression
-			case "tls_enabled":
-				TlsEnabledParse, err := strconv.ParseBool(configVal[0])
-				if err != nil {
-					return d.Errf("invalid boolean value for 'tls_enabled': %s", configVal[0])
-				}
-				rs.TlsEnabled = TlsEnabledParse
-			case "tls_insecure":
-				tlsInsecureParse, err := strconv.ParseBool(configVal[0])
-				if err != nil {
-					return d.Errf("invalid boolean value for 'tls_insecure': %s", configVal[0])
-				}
-				rs.TlsInsecure = tlsInsecureParse
-			case "tls_server_certs_pem":
-				if configVal[0] != "" {
-					rs.TlsServerCertsPEM = configVal[0]
-				}
-			case "tls_server_certs_path":
-				if configVal[0] != "" {
-					rs.TlsServerCertsPath = configVal[0]
-				}
-			case "route_by_latency":
-				routeByLatency, err := strconv.ParseBool(configVal[0])
-				if err != nil {
-					return d.Errf("invalid boolean value for 'route_by_latency': %s", configVal[0])
-				}
-				rs.RouteByLatency = routeByLatency
-			case "route_randomly":
-				routeRandomly, err := strconv.ParseBool(configVal[0])
-				if err != nil {
-					return d.Errf("invalid boolean value for 'route_randomly': %s", configVal[0])
-				}
-				rs.RouteRandomly = routeRandomly
-			}
-		}
-	}
-	return nil
-}
-
-// Provision module function called by Caddy Server
-func (rs *RedisStorage) Provision(ctx caddy.Context) error {
-
-	rs.logger = ctx.Logger()
-
-	// Abstract this logic for testing purposes
 	err := rs.finalizeConfiguration(ctx)
-	if err == nil {
+	if err != nil {
 		rs.logger.Info("Provision Redis storage", zap.String("client_type", rs.ClientType), zap.Strings("address", rs.Address))
 	}
 
@@ -172,10 +22,7 @@ func (rs *RedisStorage) Provision(ctx caddy.Context) error {
 
 func (rs *RedisStorage) finalizeConfiguration(ctx context.Context) error {
 
-	repl := caddy.NewReplacer()
-
 	for idx, v := range rs.Address {
-		v = repl.ReplaceAll(v, "")
 		host, port, err := net.SplitHostPort(v)
 		if err != nil {
 			return fmt.Errorf("invalid address: %s", v)
@@ -183,7 +30,6 @@ func (rs *RedisStorage) finalizeConfiguration(ctx context.Context) error {
 		rs.Address[idx] = net.JoinHostPort(host, port)
 	}
 	for idx, v := range rs.Host {
-		v = repl.ReplaceAll(v, defaultHost)
 		addr := net.ParseIP(v)
 		_, err := net.LookupHost(v)
 		if addr == nil && err != nil {
@@ -192,26 +38,20 @@ func (rs *RedisStorage) finalizeConfiguration(ctx context.Context) error {
 		rs.Host[idx] = v
 	}
 	for idx, v := range rs.Port {
-		v = repl.ReplaceAll(v, defaultPort)
 		_, err := strconv.Atoi(v)
 		if err != nil {
 			return fmt.Errorf("invalid port value: %s", v)
 		}
 		rs.Port[idx] = v
 	}
-	if rs.Timeout = repl.ReplaceAll(rs.Timeout, ""); rs.Timeout != "" {
+	if rs.Timeout != "" {
 		timeParse, err := strconv.Atoi(rs.Timeout)
 		if err != nil || timeParse < 0 {
 			return fmt.Errorf("invalid timeout value: %s", rs.Timeout)
 		}
 	}
-	rs.MasterName = repl.ReplaceAll(rs.MasterName, "")
-	rs.Username = repl.ReplaceAll(rs.Username, "")
-	rs.Password = repl.ReplaceAll(rs.Password, "")
-	rs.KeyPrefix = repl.ReplaceAll(rs.KeyPrefix, defaultKeyPrefix)
 
 	if len(rs.EncryptionKey) > 0 {
-		rs.EncryptionKey = repl.ReplaceAll(rs.EncryptionKey, "")
 		// Encryption_key length must be at least 32 characters
 		if len(rs.EncryptionKey) < 32 {
 			return fmt.Errorf("invalid length for 'encryption_key', must contain at least 32 bytes: %s", rs.EncryptionKey)
@@ -221,17 +61,6 @@ func (rs *RedisStorage) finalizeConfiguration(ctx context.Context) error {
 			rs.EncryptionKey = rs.EncryptionKey[:32]
 		}
 	}
-
-	rs.TlsServerCertsPEM = repl.ReplaceAll(rs.TlsServerCertsPEM, "")
-	rs.TlsServerCertsPath = repl.ReplaceAll(rs.TlsServerCertsPath, "")
-
-	// TODO: these are non-string fields so they can't easily be substituted at runtime :(
-	// rs.DB
-	// rs.Compression
-	// rs.TlsEnabled
-	// rs.TlsInsecure
-	// rs.RouteByLatency
-	// rs.RouteRandomly
 
 	// Construct Address from Host and Port if not explicitly provided
 	if len(rs.Address) == 0 {
@@ -274,52 +103,3 @@ func (rs *RedisStorage) Cleanup() error {
 
 	return nil
 }
-
-type storageConfig struct {
-	StorageRaw json.RawMessage `json:"storage,omitempty" caddy:"namespace=caddy.storage inline_key=module"`
-}
-
-func cmdRedisStorageRepair(fl caddycmd.Flags) (int, error) {
-
-	configFile := fl.String("config")
-
-	// Load configuration file (if not specified, will look in usual locations)
-	cfg, _, err := caddycmd.LoadConfig(configFile, "")
-	if err != nil {
-		return caddy.ExitCodeFailedStartup, fmt.Errorf("Unable to load config file: %v", err)
-	}
-
-	// Unmarshall the storage configuration into a temporary struct
-	var storeCfg storageConfig
-	if err := json.Unmarshal(cfg, &storeCfg); err != nil || storeCfg.StorageRaw == nil {
-		return caddy.ExitCodeFailedStartup, fmt.Errorf("Unable to unmarshal configuration: %v", err)
-	}
-
-	ctx, cancel := caddy.NewContext(caddy.Context{Context: context.Background()})
-	defer cancel()
-
-	// Load module
-	module, err := ctx.LoadModule(&storeCfg, "StorageRaw")
-	if err != nil {
-		return caddy.ExitCodeFailedStartup, err
-	}
-	// Ensure loaded module is the correct type
-	if reflect.TypeOf(module) != reflect.TypeOf(&RedisStorage{}) {
-		return caddy.ExitCodeFailedStartup, fmt.Errorf("Loaded storage module does not support Redis")
-	}
-
-	rs := module.(*RedisStorage)
-	if err := rs.Repair(ctx, ""); err != nil {
-		return caddy.ExitCodeFailedStartup, err
-	}
-
-	return caddy.ExitCodeSuccess, nil
-}
-
-// Interface guards
-var (
-	_ caddy.CleanerUpper     = (*RedisStorage)(nil)
-	_ caddy.Provisioner      = (*RedisStorage)(nil)
-	_ caddy.StorageConverter = (*RedisStorage)(nil)
-	_ caddyfile.Unmarshaler  = (*RedisStorage)(nil)
-)
--- go.mod
+++ go.mod
@@ -6,55 +6,34 @@ toolchain go1.23.3
 
 require (
 	github.com/bsm/redislock v0.9.4
-	github.com/caddyserver/caddy/v2 v2.7.6
 	github.com/caddyserver/certmagic v0.21.4
 	github.com/redis/go-redis/v9 v9.7.0
-	github.com/spf13/cobra v1.8.1
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
 )
 
 require (
-	github.com/aryann/difflib v0.0.0-20210328193216-ff5ff6dc229b // indirect
-	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/caddyserver/zerossl v0.1.3 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
-	github.com/google/pprof v0.0.0-20241128161848-dc51965c6481 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
 	github.com/libdns/libdns v0.2.2 // indirect
 	github.com/mholt/acmez/v2 v2.0.3 // indirect
 	github.com/miekg/dns v1.1.62 // indirect
-	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/onsi/ginkgo/v2 v2.22.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/prometheus/client_golang v1.20.5 // indirect
-	github.com/prometheus/client_model v0.6.1 // indirect
-	github.com/prometheus/common v0.60.1 // indirect
-	github.com/prometheus/procfs v0.15.1 // indirect
-	github.com/quic-go/qpack v0.5.1 // indirect
-	github.com/quic-go/quic-go v0.41.0 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/zeebo/blake3 v0.2.4 // indirect
-	go.uber.org/mock v0.5.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.29.0 // indirect
-	golang.org/x/exp v0.0.0-20241108190413-2d47ceb2692f // indirect
 	golang.org/x/mod v0.22.0 // indirect
 	golang.org/x/net v0.31.0 // indirect
 	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
-	golang.org/x/term v0.26.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
 	golang.org/x/tools v0.27.0 // indirect
-	google.golang.org/protobuf v1.35.2 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
